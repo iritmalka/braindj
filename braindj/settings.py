a3172d1cdbc79f5fcc7d008bf90b546159c1527c
shirilad <shirilad@gmail.com>
2013-10-12 19:21:20 +0300

    add pics

--- braindj/settings.py
+++ braindj/settings.py
@@ -61,11 +61,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/Users/shiriladelsky/braindj/static'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = '/static_zz/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
